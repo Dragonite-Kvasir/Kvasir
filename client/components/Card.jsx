700768c1480f2a2319c7aa23dcca4298bc92b13f
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2022-12-06 12:36:50 -0600

    add user card, explore

---
+++ client/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = ({name, image, willTeach, willLearn}) => {
+  return (
+    <div className="card">
+      <p className="name"></p>
+    </div>
+  )
+}
+
+export default Card;
--- client/containers/explore.jsx
+++ client/containers/explore.jsx
@@ -1,22 +1,41 @@
-const React = require('react');
-import Nav from '../components/Nav.jsx';
+import React from "react";
+import { useState, useEffect } from "react";
+import DropDown from "../components/Dropdown.jsx";
 
 const Explore = () => {
-  return (
-    <div id="explore-page">
-    <aside>
-      <div className="filter">
-        <label className="filter-heading">I want to learn: </label>
-      </div>
-      <div className="filter">
-        <label className="filter-heading">I want to teach: </label>
-      </div>
-    </aside>
-    <div className="explore-main">
+  const [learnOptions, setLearnOptions] = useState([]);
+  const [teachOptions, setTeachOptions] = useState([]);
+  const [learn, setLearn] = useState('Select a Language');
+  const [teach, setTeach] = useState('Select a Language');
+  const [profiles, setProfiles] = useState([]);
+  
+  useEffect(() => {
+    // query the database to grab matching users/profiles
+  }, [])
 
+  return ( 
+    <div id="explore-page">
+      <aside>
+        <div className="filter">
+          <label className="filter-heading">I want to learn: </label>
+          <DropDown 
+            options={learnOptions} 
+            current={learn}
+            handleChange={(e) => setLearn(e)}/>
+        </div>
+        <div className="filter">
+          <label className="filter-heading">I want to teach: </label>
+          <DropDown 
+            options={teachOptions} 
+            current={teach}
+            handleChange={(e) => setTeach(e)}/>
+        </div>
+      </aside>
+      <div className="explore-main">
+
+      </div>
     </div>
-  </div>
-  );
+  )
 };
 
 export default Explore;
