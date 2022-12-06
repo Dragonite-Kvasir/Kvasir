d677c9eb59c37221a32b8bc39ef1461628f59284
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2022-12-06 10:10:08 -0600

    add variables to scss

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div id="navbar">
+    <div id="nav-bar">
       <h1>Kvasir</h1>
+      <div id="nav-links">
+        <Links /> 
+      </div>
     </div>
   )
 }
